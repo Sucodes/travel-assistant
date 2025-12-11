a111a9dc445633fb290c90d991beeca6e3a1931c
sucodes <rhounamoney@gmail.com>
2025-12-11 22:07:51 +0000

    refactor get flights api

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 from dotenv import load_dotenv #gotten from python-dotenv package https://pypi.org/project/python-dotenv/
 import os
@@ -9,18 +9,48 @@ app = Flask(__name__)
 
 url = "https://google-flights2.p.rapidapi.com/api/v1/searchFlights" # https://rapidapi.com/DataCrawler/api/google-flights2/playground/apiendpoint_ce4a44ea-f781-4baf-883f-ea1b7da10907
 
-querystring = {"departure_id":"LAX","arrival_id":"JFK", "outbound_date": "2025-12-21","travel_class":"ECONOMY","adults":"1","show_hidden":"1","currency":"USD","language_code":"en-US","country_code":"US","search_type":"best"}
+@app.route("/api/flights")
+def get_flights():
+    headers = {
+        "x-rapidapi-key": os.getenv("RAPID_API_KEY"),
+        "x-rapidapi-host": os.getenv("RAPID_API_HOST")
+    }
 
-headers = {
-	"x-rapidapi-key": os.getenv("RAPID_API_KEY"),
-	"x-rapidapi-host": os.getenv("RAPID_API_HOST")
-}
+    departure_id = request.args.get("departure_id")
+    arrival_id = request.args.get("arrival_id")
+    outbound_date = request.args.get("outbound_date")
+    inbound_date = request.args.get("inbound_date")
+    adults = request.args.get("adults")
 
-response = requests.get(url, headers=headers, params=querystring)
+    querystring = {
+        "departure_id": departure_id,
+        "arrival_id": arrival_id,
+        "outbound_date": outbound_date,
+        "inbound_date": inbound_date,
+        "travel_class": "ECONOMY",
+        "adults": adults,
+        "search_type": "best",
+    }
 
-@app.route("/")
-def hello_world():
-    return jsonify(response.json())
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            params=querystring
+        )
+
+        if response.status_code == 200:
+            flights = response.json()
+            return jsonify(flights.data)
+        else:
+            print('Error:', response.status_code, response.text)
+            return jsonify({"error": "Failed to contact flight API", "details": response.text}), response.status_code, response.status_code, {
+                "Access-Control-Allow-Origin": "*"
+            }
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        return jsonify({"error": "Failed to contact API"})
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
