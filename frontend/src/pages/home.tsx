da3e984df498cd61095681f8fb27fb740635de58
sucodes <rhounamoney@gmail.com>
2025-12-12 02:21:28 +0000

    feat: update form with date picker

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,5 +1,8 @@
 import { useForm, type SubmitHandler } from "react-hook-form";
 import styles from "./Home.module.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 type FlightType = "returnFlight" | "oneWayFlight";
 
@@ -7,18 +10,30 @@ interface FormData {
   flightType: FlightType;
   departure: string;
   destination: string;
-  departureDate: string;
-  arrivalDate: string;
+  departureDate: string | null;
+  arrivalDate: string | null;
   passengers: number;
 }
 
 const Home = () => {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(startDate);
   const {
     register,
     handleSubmit,
     watch,
+    setValue,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({
+    defaultValues: {
+      flightType: "oneWayFlight",
+      departureDate: null,
+      arrivalDate: null,
+      destination: "",
+      departure: "",
+      passengers: 0,
+    },
+  });
 
   const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
 
@@ -35,10 +50,7 @@ const Home = () => {
       <div className={styles.formContainer}>
         <div className={styles.formHeader}>
           <h1>Find Your Perfect Flight</h1>
-          <p>
-            Your travel assistance, we've got you covered when
-            plans change
-          </p>
+          <p>Your travel assistance, we've got you covered when plans change</p>
         </div>
 
         <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
@@ -80,7 +92,20 @@ const Home = () => {
                   ? "Enter Departure Date"
                   : "Enter Date"}
               </label>
-              <input {...register("departureDate", { required: true })} />
+              <DatePicker
+                placeholderText="Click to select a date"
+                dateFormat="yyyy-MM-dd"
+                minDate={new Date()}
+                selected={startDate}
+                onChange={(date) => {
+                  setStartDate(date);
+                  if (startDate)
+                    setValue(
+                      "departureDate",
+                      startDate.toISOString().split("T")[0]
+                    );
+                }}
+              />
               {errors.departureDate && (
                 <span className={styles.errorMessage}>
                   This field is required
@@ -91,10 +116,18 @@ const Home = () => {
             {flightTypeToggle === "returnFlight" && (
               <div className={styles.inputGroup}>
                 <label>Enter arrival date</label>
-                <input
-                  {...register("arrivalDate", {
-                    required: flightTypeToggle === "returnFlight",
-                  })}
+                <DatePicker
+                  minDate={new Date()}
+                  dateFormat="yyyy-MM-dd"
+                  selected={endDate}
+                  onChange={(date) => {
+                    setEndDate(date);
+                    if (endDate)
+                      setValue(
+                        "arrivalDate",
+                        endDate.toISOString().split("T")[0]
+                      );
+                  }}
                 />
                 {errors.arrivalDate && (
                   <span className={styles.errorMessage}>
