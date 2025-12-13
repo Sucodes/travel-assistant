868c1e4045f9c5469774c6e03ca2135bee511411
sucodes <rhounamoney@gmail.com>
2025-12-13 22:05:32 +0000

    add fixture to use for tests (https://flask.palletsprojects.com/en/latest/testing/)

---
+++ backend/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from main import create_app
+from models import db
+
+@pytest.fixture
+def app():
+    app = create_app({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+    })
+
+    with app.app_context():
+        db.create_all()
+
+    yield app
+
+    with app.app_context():
+        db.session.remove()
+        db.drop_all()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
---
+++ backend/tests/test_flight_bookings.py
@@ -0,0 +1,25 @@
+TEST_FLIGHT_BOOKING_DATA = {
+    "duration_label": "7 hr 11 min",
+    "duration": 431,
+    "airline": "JetBlue",
+    "airline_logo": "https://www.gstatic.com/flights/airline_logos/70px/B6.png",
+    "flight_number": "B6 1107",
+    "departure_city": "John F. Kennedy International Airport",
+    "departure_code": "JFK",
+    "departure_time": "2025-2-1 08:34",
+    "arrival_city": "Heathrow Airport",
+    "arrival_code": "LHR",
+    "arrival_time": "2025-2-1 20:45",
+    "price": "569",
+    "stops": 0
+}
+
+def test_create_function_for_flight_booking_returns_201(client):
+    response = client.post("/api/flight-bookings", json=TEST_FLIGHT_BOOKING_DATA)
+
+    assert response.status_code == 201
+
+    data = response.get_json()
+    assert "id" in data
+    assert data["departure_city"] == "John F. Kennedy International Airport"
+    assert data["arrival_city"] == "Heathrow Airport"
