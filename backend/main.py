cc75b6080798197dcdffc8ddebcd8d18279c49cc
sucodes <rhounamoney@gmail.com>
2025-12-13 14:29:15 +0000

    feat: add database model

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ import os
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+
 load_dotenv()
 
 class Base(DeclarativeBase):
@@ -18,6 +19,43 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
 
 url = "https://google-flights2.p.rapidapi.com/api/v1/searchFlights" # https://rapidapi.com/DataCrawler/api/google-flights2/playground/apiendpoint_ce4a44ea-f781-4baf-883f-ea1b7da10907
+
+class Flight_Bookings(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    airline = db.Column(db.String(), nullable=False)
+    airline_logo = db.Column(db.String(), nullable=True)
+    flight_number = db.Column(db.String(), nullable=False)
+    departure_time = db.Column(db.String(), nullable=False)
+    departure_city = db.Column(db.String(), nullable=False)
+    departure_code = db.Column(db.String(), nullable=False)
+    arrival_time = db.Column(db.String(), nullable=False)
+    arrival_city = db.Column(db.String(), nullable=False)
+    arrival_code = db.Column(db.String(), nullable=False)
+    duration = db.Column(db.Integer(), nullable=False)
+    stops = db.Column(db.Integer(), nullable=False, default=0)
+    price = db.Column(db.String(), nullable=False)
+    passengers = db.Column(db.Integer, nullable=False, default=1)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "airline": self.airline,
+            "airline_logo": self.airline_logo,
+            "flight_number": self.flight_number,
+            "departure_time": self.departure_time,
+            "departure_city": self.departure_city,
+            "departure_code": self.departure_code,
+            "arrival_time": self.arrival_time,
+            "arrival_city": self.arrival_city,
+            "arrival_code": self.arrival_code,
+            "duration": self.duration,
+            "stops": self.stops,
+            "price": self.price,
+            "passengers": self.passengers
+        }
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/api/flights")
 def get_flights():
